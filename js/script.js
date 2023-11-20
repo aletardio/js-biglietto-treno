7339d6c9dbcc010cbf580bf1d960ea2d65b54daf
aletardio <ale.tardio99@gmail.com>
2023-11-20 15:32:03 +0100

    Added bonus with [.getElementById].innerHTML

--- js/script.js
+++ js/script.js
@@ -10,17 +10,25 @@ let price_final = '';
 if (age <= 17) {
     let sale = price_start * 20 / 100;
     price_final = price_start - sale;
+    ticket_price = `Hai diritto ad uno sconto! Il prezzo del tuo biglietto
+    è di: ${price_final.toFixed(2)}€`;
 }
 
 else if (age >= 65) {
     let sale = price_start * 40 / 100;
     price_final = price_start - sale;
+    ticket_price = `Hai diritto ad uno sconto! Il prezzo del tuo biglietto
+    è di: ${price_final.toFixed(2)}€`;
 }
 
 else {
     price_final = price_start;
+    ticket_price = `Non hai diritto ad uno sconto! Il prezzo del tuo biglietto
+    è di: ${price_final.toFixed(2)}€`;
 }
 
-console.log(price_final);
+document.getElementById('train-ticket').innerHTML = ticket_price;
+
+console.log(ticket_price);
 
 
