227b379bb7a99ff9efb1ee287bcfda5a6f3117f7
aletardio <ale.tardio99@gmail.com>
2023-11-20 15:03:26 +0100

    added variables with if and else

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+let miles = prompt('Inserisci il numero di chilometri che vuoi percorrere');
+
+let age = prompt('Inserisci la tua età');
+
+let ticket = 0.21;
+
+
+if (age <= 17) {
+    ticket = ticket * miles - 20 * 100;
+}
+
+else if (age >= 65) {
+    ticket = ticket * miles - 40 * 100;
+}
+
+else {
+    ticket = '';
+}
+
+console.log(ticket);
+
 
