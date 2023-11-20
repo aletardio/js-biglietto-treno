a6b2c3df1dc313bbf1b3722a11fe2c5ee22cda97
aletardio <ale.tardio99@gmail.com>
2023-11-20 15:16:41 +0100

    fixed operations

--- js/script.js
+++ js/script.js
@@ -2,21 +2,25 @@ let miles = prompt('Inserisci il numero di chilometri che vuoi percorrere');
 
 let age = prompt('Inserisci la tua età');
 
-let ticket = 0.21;
+let price_start = 0.21 * miles;
+
+let price_final = '';
 
 
 if (age <= 17) {
-    ticket = ticket * miles - 20 * 100;
+    let sale = price_start * 20 / 100;
+    price_final = price_start - sale;
 }
 
 else if (age >= 65) {
-    ticket = ticket * miles - 40 * 100;
+    let sale = price_start * 40 / 100;
+    price_final = price_start - sale;
 }
 
 else {
-    ticket = '';
+    price_final = price_start;
 }
 
-console.log(ticket);
+console.log(price_final);
 
 
